edd2b07fcd0a36dff2099930620290dacd82b755
Volha.Bondarava <vol.bondarava@gmail.com>
2018-11-06 14:46:01 +0300

    Add check for result's quantity

--- spec/test/SearchTestSpec.js
+++ spec/test/SearchTestSpec.js
@@ -13,19 +13,24 @@ describe('Test without Page Object', function () {
     await driver.quit()
   })
 
-  it('Search Test', async function () {
-    setTimeout(async function () {
-      await driver.get('http://google.by')
-    }, 9000)
+  it('Results quantity Test', async function () {
+    await driver.get('http://google.by')
+    setTimeout(function () {
 
+    }, 6000)
     await driver.findElement(By.name('q')).sendKeys('iTechArt', Key.RETURN)
     await driver.wait(until.titleIs('iTechArt - Пошук Google'))
 
     let text = await driver.findElement(By.id('resultStats')).getText()
     text = text.substring(0, text.length - 11)
     let number = parseInt(text.replace(/\D+/g, ''))
+    if (number < 30000) {
+      fail('There is few results (fewer than 6660) ')
+    }
     console.log(`Количество результатов: ${number}`)
+  })
 
+  it('Relevance of results Test', async function f () {
     let values = await driver.findElements(By.xpath('//div[@id=\'search\']//h3'))
     values.forEach(async function (element) {
       let result = await element.getText()
--- spec/test/SearchTestSpecPO.js
+++ spec/test/SearchTestSpecPO.js
@@ -15,14 +15,21 @@ describe('Test with Page Object', function () {
     await driver.quit()
   })
 
-  it('Search Test', async function () {
-    setTimeout(async function () {
-      await googlePage.open()
-    })
+  it('Results quantity Test', async function () {
+    await googlePage.open()
+    setTimeout(function () {
+
+    }, 10000)
     await googlePage.typeSearchQuery('iTechArt')
     await googlePage.clickSearchButton()
     let number = await googlePage.getQuantityOfSearchResults()
+    if (number < 30000) {
+      fail('There is few results (fewer than 6660) ')
+    }
     console.log(`There is about ${number} results.`)
+  })
+
+  it('Relevance of results Test', async function () {
     let results = await googlePage.getSearchResults()
     results.forEach(async function (element) {
       let result = await element.getText()
