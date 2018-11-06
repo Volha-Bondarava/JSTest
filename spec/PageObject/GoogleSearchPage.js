8487d7a48ab405a2e87fca16325962756651b0db
Volha.Bondarava <vol.bondarava@gmail.com>
2018-11-06 12:53:29 +0300

    Update GoogleSearchPage with use of class definition

--- spec/PageObject/GoogleSearchPage.js
+++ spec/PageObject/GoogleSearchPage.js
@@ -1,38 +1,38 @@
 const {By, until} = require('selenium-webdriver');
 let webdriver = require('selenium-webdriver');
 
-GoogleSearchPage = function GoogleSearchPage(driver) {
-    this.driver = driver;
-    this.url = "http://google.by";
-    this.searchField = webdriver.By.name('q');
-    this.resultStats = webdriver.By.id('resultStats');
-    this.searchResults = webdriver.By.xpath('//h3');
-    this.searchButton = webdriver.By.name('btnK');
-};
+class GoogleSearchPage {
 
-GoogleSearchPage.prototype.open = function () {
-    return this.driver.get(this.url);
-};
+    constructor(driver) {
+        this.driver = driver;
+        this.url = "http://google.by";
+        this.searchField = webdriver.By.name('q');
+        this.resultStats = webdriver.By.id('resultStats');
+        this.searchResults = webdriver.By.xpath("//div[@id='search']//h3");
+        this.searchButton = webdriver.By.name('btnK');
+    };
 
-GoogleSearchPage.prototype.typeSearchQuery = function (text) {
-    return this.driver.findElement(this.searchField).sendKeys(text);
-};
+    open() {
+        return this.driver.get(this.url);
+    };
 
-GoogleSearchPage.prototype.clickSearchButton = async function() {
-    await this.driver.findElement(this.searchButton).click();
-    return this.driver.wait(until.titleIs('iTechArt - Пошук Google'));
-};
+    typeSearchQuery = function (text) {
+        return this.driver.findElement(this.searchField).sendKeys(text);
+    };
 
-GoogleSearchPage.prototype.getQuantityOfSearchResults = async function() {
-    let text = await this.driver.findElement(By.id('resultStats')).getText();node_
-    text = text.substring(0, text.length - 11);
-    return parseInt(text.replace(/\D+/g, ""));
-};
+    clickSearchButton = async function() {
+        await this.driver.findElement(this.searchButton).click();
+        return this.driver.wait(until.titleIs('iTechArt - Пошук Google'));
+    };
 
-GoogleSearchPage.prototype.getSearchResults = async function() {
-    let results = await this.driver.findElements(this.searchResults);
-    results.splice(-1, 1);
-    return results;
-};
+    getQuantityOfSearchResults = async function() {
+        let text = await this.driver.findElement(By.id('resultStats')).getText();
+        text = text.substring(0, text.length - 11);
+        return parseInt(text.replace(/\D+/g, ""));
+    };
 
+    getSearchResults = async function() {
+        return await this.driver.findElements(this.searchResults);
+    };
+}
 module.exports = GoogleSearchPage;
