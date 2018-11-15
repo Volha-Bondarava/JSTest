9ea52d2fe9e83053116fb7227f7c91301ee8b686
Volha.Bondarava <vol.bondarava@gmail.com>
2018-11-15 10:54:59 +0300

    Add gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,7 @@
 const gulp = require('gulp')
 const gulp_jasmine = require('gulp-jasmine')
-let Jasmine = require('jasmine')
-let jasmine = new Jasmine()
 
 gulp.task('default', async function () {
-  return gulp.src('../spec/test/SearchTestSpecPO.js')
-    .pipe(gulp_jasmine({
-      config: jasmine.loadConfigFile('jasmine.js'),
-      timeout: 15000,
-/*      config: [
-        jasmine.loadConfig({
-          spec_dir: 'spec',
-          spec_files: [
-            '../test/SearchTestSpecPO.js'
-          ]
-        }),
-        jasmine.randomizeTests(false)
-      ]*/
-    }))
+  return await gulp.src('spec/support/jasmine.js')
+    .pipe(gulp_jasmine())
 })
