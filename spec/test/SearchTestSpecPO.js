62fc99bfcc4dd4cfdf066d7bac65122ae0678ffd
Volha.Bondarava <vol.bondarava@gmail.com>
2018-11-16 14:53:03 +0300

    Update SearchTestSpecPO.js

--- spec/test/SearchTestSpecPO.js
+++ spec/test/SearchTestSpecPO.js
@@ -1,35 +1,39 @@
 let using = require('jasmine-data-provider')
 let provider = require('../properties/data.json')
 let GoogleSearchPage = require('../PageObject/GoogleSearchPage.js')
+let GoogleSearchResultsPage = require('../PageObject/GoogleSearchResultsPage.js')
 require('chromedriver')
 let webdriver = require('selenium-webdriver')
 
 describe('Test with Page Object', function () {
-  let googlePage
+  let googleBasePage, googleResultsPage
 
   beforeAll(async function () {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000
     let driver = await new webdriver.Builder().forBrowser('chrome').build()
     await driver.manage().setTimeouts({implicit: 15000, pageLoad: 15000})
-    googlePage = new GoogleSearchPage(driver)
+    googleBasePage = new GoogleSearchPage(driver)
   })
 
-  afterAll(async function () {
-    await googlePage.quitDriver()
+  afterAll(function () {
+    googleBasePage.close()
+    googleResultsPage.close()
   })
 
   using(provider, function (data) {
     it('Results quantity Test', async function () {
-      await googlePage.open()
-      await googlePage.searchQuery(data.query)
+      await googleBasePage.open()
+      await googleBasePage.searchQuery(data.query)
 
-      let number = await googlePage.getQuantityOfSearchResults()
+      googleResultsPage = new GoogleSearchResultsPage(googleBasePage.driver.getDriver())
+
+      let number = await googleResultsPage.getQuantityOfSearchResults()
       await expect(number).toBeGreaterThan(data.resultsNumber)
       console.log(`Query: ${data.query}. There is about ${number} results.`)
     })
 
     it('Relevance of results Test', async function () {
-      let results = await googlePage.getSearchResults()
+      let results = await googleResultsPage.getSearchResults()
       await expect(results.length).toBeGreaterThan(0)
       results.forEach(function (result) {
         let reg = RegExp(data.regexp)
