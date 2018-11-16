1c23350ea156f382811529491ed6d759b279e9aa
Volha.Bondarava <vol.bondarava@gmail.com>
2018-11-16 17:30:29 +0300

    Update spec without PO

--- spec/test/SearchTestSpec.js
+++ spec/test/SearchTestSpec.js
@@ -6,38 +6,40 @@ let webdriver = require('selenium-webdriver')
 describe('Test without Page Object', function () {
   let driver
 
+  let searchFieldName = 'q'
+  let resultStatsId = 'resultStats'
+  let searchResultsXpath = '//div[@id=\'search\']//a/h3'
+
   beforeAll(async function () {
-    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000
-    this.driver = await new webdriver.Builder().forBrowser('chrome').build()
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000
+    driver = await new webdriver.Builder().forBrowser('chrome').build()
   })
 
   afterAll(async function () {
-    await this.driver.quit()
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout
+    await driver.quit()
   })
 
   using(provider, function (data) {
     it('Results quantity Test', async function () {
-      await this.driver.get('http://google.by')
-      await this.driver.wait(until.elementLocated(By.name('q')), 10000)
-      await this.driver.findElement(By.name('q')).sendKeys(data.query, Key.RETURN)
-      await this.driver.wait(until.elementLocated(By.id('resultStats')), 10000)
+      await driver.get('http://google.by')
+      await driver.wait(until.elementLocated(By.name(searchFieldName)), 15000)
+      await driver.findElement(By.name(searchFieldName)).sendKeys(data.query, Key.RETURN)
+      await driver.wait(until.elementLocated(By.id(resultStatsId)), 15000)
 
-      let text = await this.driver.findElement(By.id('resultStats')).getText()
+      let text = await driver.findElement(By.id(resultStatsId)).getText()
       text = text.substring(0, text.length - 11)
       let number = parseInt(text.replace(/\D+/g, ''))
-      if (number < data.resultsNumber) {
-        fail(`There is few results (fewer than ${data.resultsNumber}) `)
-      }
+      await expect(number).toBeGreaterThan(data.resultsNumber)
       console.log(`Запрос: ${data.query}. Количество результатов: ${number}`)
     })
 
     it('Relevance of results Test', async function () {
-      let values = await this.driver.findElements(By.xpath('//div[@id=\'search\']//a/h3'))
+      let values = await driver.findElements(By.xpath(searchResultsXpath))
+
       values.forEach(async function (element) {
         let result = await element.getText()
-        expect(result.includes(data.query)).toBeTruthy()
+        let reg = RegExp(data.regexp)
+        await expect(reg.test(result)).toBeTruthy(`act: ${result}, exp: ${data.query}`)
       })
     })
 
